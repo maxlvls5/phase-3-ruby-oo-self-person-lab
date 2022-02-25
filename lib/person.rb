c0a1567d990df34f70990708f88bd872ffc89b2b
Shane <MalithSM@hotmail.com>
2022-02-24 22:08:48 -0600

    person lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,45 @@
 # your code goes here
+require 'pry'
+
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name 
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def  happiness=(num)
+        @happiness = if num > 10
+                    10
+        elsif num < 0
+            0
+        else
+            num 
+        end
+    end
+
+
+
+    def  hygiene=(num)
+        @hygiene = if num > 10
+                    10
+        elsif num < 0
+            0
+        else
+            num 
+        end
+    end
+
+
+
+
+
+end
+
+p1 = Person.new('name')
+
+binding.pry
